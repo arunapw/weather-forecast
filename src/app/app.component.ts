031cfa757c1b98523c8b62507c2d7a8307d4cbc1
Aruna Wankhade <shubham.wankhade@locationguru.com>
2023-03-25 02:23:50 +0530

    #10 : work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-forecast';
+
+  createAndObserve() {
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('hidden');
+    hiddenElements.forEach((e) => {
+      observer.observe(e);
+    });
+
+  }
+
 }
